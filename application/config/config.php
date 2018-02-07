ab7a6fa3106b75e868b38d062afaa508ff294f6e
Veris Juniardi <Veris Juniardi>
2018-02-08 03:42:38 +0700

    migrate data

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-// $config['base_url'] = 'https://gantung.si-desa.id/';
-$config['base_url'] = 'http://localhost/SIA-GTG.github/';
+$config['base_url'] = 'https://gantung.si-desa.id/';
+// $config['base_url'] = 'http://localhost/SIA-GTG.github/';
 
 /*
 |--------------------------------------------------------------------------
