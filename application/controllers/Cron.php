cdf7458da252670e49ac2c52ae46ac31a603a34f
veris juniardi <veris.juniardi@gmail.com>
2018-08-07 02:05:31 +0700

    up

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -17,7 +17,8 @@ class Cron extends CI_Controller{
       $datestring = '%d %M %Y - %h:%i %a';
       $time = time();
       $sekarang = mdate($datestring, $time);
-      $to = '082281469926';
+      // $to = '082281469926';
+      $to = '082269200372';
       $message = 'Ini Pesan Dari '.$server.' Cron Sistem Pada : '.$sekarang;
       sms_notifikasi($to, $message);
     // }else {
