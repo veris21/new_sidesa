4f6ae78ae4859328e15abc7d5e974926936f3f70
veris juniardi <veris.juniardi@gmail.com>
2018-08-06 15:46:55 +0700

    update

--- application/views/pertanahan_layout/print/surat_tanah.php
+++ application/views/pertanahan_layout/print/surat_tanah.php
@@ -70,7 +70,7 @@
         
         <tr>
             <td>Alamat</td>
-            <td colspan="3" align="left">: <?php echo $data['alamat'].", ".$desa['name'].", ".$kecamatan['name'].", ".$kabupaten['name'].", ".$provinsi['name'];?></td>
+            <td colspan="3" align="left">: <?php echo $data['alamat'].", ".$desa['name'].$data['desa_penduduk_id'].", ".$kecamatan['name'].", ".$kabupaten['name'].", ".$provinsi['name'];?></td>
         </tr>
         <tr>
             <td colspan="4"></td>
