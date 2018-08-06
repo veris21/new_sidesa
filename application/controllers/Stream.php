6cbcc12c3b7dda530b700f0a204957d425fb222d
veris juniardi <veris.juniardi@gmail.com>
2018-08-07 05:11:30 +0700

    nun

--- application/controllers/Stream.php
+++ application/controllers/Stream.php
@@ -62,8 +62,8 @@ class Stream extends CI_Controller{
         break;
     }
     $data['penduduk'] = array(
-      "id"=> floatval($tanah['nik']),
-      "tanah_id"=> floatval($tanah['id']),
+      "id"=> (float)($tanah['nik']),
+      "tanah_id"=> (float)($tanah['id']),
       "nama"=>$penduduk['nama'],
       // "kk"=>substr_replace($penduduk['no_kk'], '********', 8),
       "tempat_tanggal_lahir"=>$penduduk['tempat_lahir'].",".$penduduk['tanggal_lahir'],
