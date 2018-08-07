1b1fa04729b0cab0ce4db49fac3a73ed6988803b
veris juniardi <veris.juniardi@gmail.com>
2018-08-08 02:01:49 +0700

    up

--- application/views/pertanahan_layout/detail_berita_acara.php
+++ application/views/pertanahan_layout/detail_berita_acara.php
@@ -405,12 +405,22 @@ function initialize() {
         fillOpacity: 0.3,
         html: contentString
     });
-    
-    marker = new google.maps.marker({
-        position: mark,
-        label: labels[labelIndex++ % labels.length],
-        map: map
-    });
+        var locations = patok;
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) { 
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
 
     polygon.setMap(map);
     marker.setMap(map);
