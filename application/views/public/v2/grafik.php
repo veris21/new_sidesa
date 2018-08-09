2b42832d4f78e9a09bac7bd18973239643ccc3ad
veris juniardi <veris.juniardi@gmail.com>
2018-08-09 15:15:48 +0700

    up

--- application/views/public/v2/grafik.php
+++ application/views/public/v2/grafik.php
@@ -13,6 +13,7 @@
             <div id="pendidikan" style="width:100%; height:400px;"></div>
         </div>        
     </div>
+    <hr>
     <div class="row">
         <div class="col-md-12">
         <div id="pekerjaan" style="width:100%; height:400px;"></div>
