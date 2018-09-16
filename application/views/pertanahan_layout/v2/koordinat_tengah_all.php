3d609f3c9f5fd86eda20955aa4ed781090e24aeb
veris juniardi <veris.juniardi@gmail.com>
2018-09-16 12:30:04 +0700

    up

--- application/views/pertanahan_layout/v2/koordinat_tengah_all.php
+++ application/views/pertanahan_layout/v2/koordinat_tengah_all.php
@@ -378,17 +378,6 @@ $.ajax({
        });
        color = '#'+Math.random().toString(16).substr(-6);
        add_poly(path, color);
-    //    console.log(path);
-    //    color.push('#'+Math.random().toString(16).substr(-6));
-    //    var polygon = new google.maps.Polygon({
-    //       paths: path,
-    //       strokeColor: '#000',
-    //       strokeOpacity: 0.8,
-    //       strokeWeight: 2,
-    //       fillColor: color,
-    //       fillOpacity: 0.35
-    //     });
-    //    polygon.setMap(map);
     }
 });
 
@@ -471,16 +460,18 @@ $.ajax({
                 // console.log("Data Adm : " +data_adm);
                 path_adm.push(data_adm);
             });
-            console.log(path_adm);
-            var polygonAdm = new google.maps.Polygon({
-                paths: path_adm,
-                strokeColor: '#000',
-                strokeOpacity: 0.5,
-                strokeWeight: 1,
-                fillColor: "#ffffff",
-                fillOpacity: 0.0
-                });
-            polygonAdm.setMap(map);
+            color = '#ff8000';
+            add_poly(path_adm, color)
+            // console.log(path_adm);
+            // var polygonAdm = new google.maps.Polygon({
+            //     paths: path_adm,
+            //     strokeColor: '#000',
+            //     strokeOpacity: 0.5,
+            //     strokeWeight: 1,
+            //     fillColor: "#ffffff",
+            //     fillOpacity: 0.0
+            //     });
+            // polygonAdm.setMap(map);
         }
     });
 
