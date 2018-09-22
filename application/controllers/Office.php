935b6fa907792b8602bbc22e7922724c385ec034
veris juniardi <veris.juniardi@gmail.com>
2018-09-23 02:32:01 +0700

    up

--- application/controllers/Office.php
+++ application/controllers/Office.php
@@ -113,24 +113,24 @@ class Office extends CI_Controller{
 
     $subject = 'This is a test';
     $message = '<p>This message has been sent for testing purposes.</p>';
-
+    $body = 'Percobaan Email';
     // Get full html:
-    $body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=' . strtolower(config_item('charset')) . '" />
-        <title>' . html_escape($subject) . '</title>
-        <style type="text/css">
-            body {
-                font-family: Arial, Verdana, Helvetica, sans-serif;
-                font-size: 16px;
-            }
-        </style>
-    </head>
-    <body>
-    ' . $message . '
-    </body>
-    </html>';
+    // $body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    // <html xmlns="http://www.w3.org/1999/xhtml">
+    // <head>
+    //     <meta http-equiv="Content-Type" content="text/html; charset=' . strtolower(config_item('charset')) . '" />
+    //     <title>' . html_escape($subject) . '</title>
+    //     <style type="text/css">
+    //         body {
+    //             font-family: Arial, Verdana, Helvetica, sans-serif;
+    //             font-size: 16px;
+    //         }
+    //     </style>
+    // </head>
+    // <body>
+    // ' . $message . '
+    // </body>
+    // </html>';
     // Also, for getting full html you may use the following internal method:
     //$body = $this->email->full_html($subject, $message);
 
