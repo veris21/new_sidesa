90fde2e360677880dda6fed4bcd67968dc3b1ee3
veris juniardi <veris.juniardi@gmail.com>
2018-09-23 04:07:13 +0700

    up

--- application/config/email.php
+++ application/config/email.php
@@ -7,12 +7,12 @@ $config['smtp_host']        = 'smtp.gmail.com';
 $config['smtp_auth']        = true;                     // Whether to use SMTP authentication, boolean TRUE/FALSE. If this option is omited or if it is NULL, then SMTP authentication is used when both $config['smtp_user'] and $config['smtp_pass'] are non-empty strings.
 $config['smtp_user']        = 'id.sidesa@gmail.com';
 $config['smtp_pass']        = 'sidesagantungproject';
-$config['smtp_port']        = 587;                       // 587 or 465
+$config['smtp_port']        = 465;                       // 587 or 465
 $config['smtp_timeout']     = 30;                       // (in seconds)
-$config['smtp_crypto']      = 'ssl';                    // '' or 'tls' or 'ssl'
+$config['smtp_crypto']      = 'tls';                    // '' or 'tls' or 'ssl'
 $config['smtp_debug']       = 0;                        // PHPMailer's SMTP debug info level: 0 = off, 1 = commands, 2 = commands and data, 3 = as 2 plus connection status, 4 = low level data output.
 $config['debug_output']     = '';                       // PHPMailer's SMTP debug output: 'html', 'echo', 'error_log' or user defined function with parameter $str and $level. NULL or '' means 'echo' on CLI, 'html' otherwise.
-$config['smtp_auto_tls']    = true;                    // Whether to enable TLS encryption automatically if a server supports it, even if `smtp_crypto` is not set to 'tls'.
+$config['smtp_auto_tls']    = false;                    // Whether to enable TLS encryption automatically if a server supports it, even if `smtp_crypto` is not set to 'tls'.
 $config['smtp_conn_options'] = array();                 // SMTP connection options, an array passed to the function stream_context_create() when connecting via SMTP.
 $config['wordwrap']         = true;
 $config['wrapchars']        = 76;
