75005794619c77fecaaf35436711626609fab840
veris juniardi <veris.juniardi@gmail.com>
2018-09-28 03:13:49 +0700

    up

---
+++ application/controllers/version-2/template.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Template extends CI_Controller {
+
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file Template.php */
